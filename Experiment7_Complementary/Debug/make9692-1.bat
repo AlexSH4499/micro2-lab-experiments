8b73efa992237c82d632bc10268ce1a7c0cba6b3
Reynaldo Belfort <Reynaldo Belfort>
2017-04-22 15:31:59 -0400

    Fixes to the code. Alarm is now working. Requires some extra fixes on LCD

--- Experiment7_Complementary/Debug/make9692-1.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-"C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.2/bin/armcl" -mv4 --code_state=32 --abi=ti_arm9_abi --include_path="C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.2/include" --include_path="C:/ti/TivaWare_C_Series-2.1.4.178" --include_path="C:/Users/reyna/Documents/Code Composer/Micro2 Lab Workspace/r-tiles-dev/custom-libraries" -g --display_error_number --diag_warning=225 --diag_wrap=off --preproc_with_compile --preproc_dependency="MIL_LCD_lib.pp"  "C:/Users/reyna/Documents/Code Composer/Micro2 Lab Workspace/micro2-lab-experiments/custom-libraries/MIL_LCD_lib.c"
